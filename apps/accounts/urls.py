d42d4a709f1328923bbe68bf294e4988e7d7dfd2
Dieu Merci <dieumercin21@gmail.com>
2026-05-02 04:12:14 +0200

    Fix TokenRefreshView import and ensure OTP utilities

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView
-from .views import RegisterView, UserProfileView, UpdateProfileView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, UserProfileView, UpdateProfileView, CustomTokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
