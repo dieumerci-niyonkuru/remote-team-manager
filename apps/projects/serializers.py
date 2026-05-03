bc3b50b6641456ed18fc72d2b7cb14512299ba55
Dieu Merci <dieumercin21@gmail.com>
2026-05-03 02:19:53 +0200

    Minimal working backend: only essential models (Project, Task) to fix deployment

--- apps/projects/serializers.py
+++ apps/projects/serializers.py
@@ -1,38 +1,8 @@
 from rest_framework import serializers
-from .models import Project, Task, Subtask, Comment, Suggestion, Reaction
+from .models import Project, Task
 from apps.accounts.serializers import UserSerializer
 
-class SubtaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subtask
-        fields = '__all__'
-
-class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    class Meta:
-        model = Comment
-        fields = '__all__'
-        read_only_fields = ('user',)
-
-class SuggestionSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    class Meta:
-        model = Suggestion
-        fields = '__all__'
-        read_only_fields = ('user',)
-
-class ReactionSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    class Meta:
-        model = Reaction
-        fields = '__all__'
-        read_only_fields = ('user',)
-
 class TaskSerializer(serializers.ModelSerializer):
-    subtasks = SubtaskSerializer(many=True, read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
-    suggestions = SuggestionSerializer(many=True, read_only=True)
-    reactions = ReactionSerializer(many=True, read_only=True)
     assignee = UserSerializer(read_only=True)
     class Meta:
         model = Task
@@ -45,9 +15,3 @@ class ProjectSerializer(serializers.ModelSerializer):
         model = Project
         fields = '__all__'
         read_only_fields = ('created_by',)
-
-class ReactionSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    class Meta:
-        model = Reaction
-        fields = ('id', 'user', 'emoji', 'created_at')
